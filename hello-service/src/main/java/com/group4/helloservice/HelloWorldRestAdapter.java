b18ef180a4ad65675b02563e4efdfa4e2bdb46f6
pawel0802 <pawel0802@gmail.com>
2025-11-17 15:13:30 +0100

    add hello-world-service config

--- hello-service/src/main/java/com/group4/helloservice/HelloWorldRestAdapter.java
+++ hello-service/src/main/java/com/group4/helloservice/HelloWorldRestAdapter.java
@@ -1,4 +1,4 @@
-package com.group4.helloworldservice.adapters.rest;
+package com.group4.helloservice;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello-world")
 @RestController
 public class HelloWorldRestAdapter {
+
+    public HelloWorldRestAdapter() {
+    }
 
     @GetMapping
     public ResponseEntity<String> getHello() {
